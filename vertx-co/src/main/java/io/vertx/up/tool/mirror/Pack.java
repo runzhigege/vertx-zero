8dc70f30bcc6df92408a5460c0beb950e109133a
Lang <silentbalanceyh@126.com>
2017-12-07 15:47:56 +0800

    Package Scanner finished

--- vertx-co/src/main/java/io/vertx/up/tool/mirror/Pack.java
+++ vertx-co/src/main/java/io/vertx/up/tool/mirror/Pack.java
@@ -1,11 +1,9 @@
 package io.vertx.up.tool.mirror;
 
-import io.vertx.core.impl.ConcurrentHashSet;
 import io.vertx.up.log.Annal;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.CountDownLatch;
 import java.util.function.Predicate;
 
 /**
@@ -14,8 +12,6 @@ import java.util.function.Predicate;
 public final class Pack {
 
     private static final Annal LOGGER = Annal.get(Pack.class);
-
-    private static final Set<Class<?>> CLASSES = new ConcurrentHashSet<>();
 
     private static final Set<String> FORBIDDEN = new HashSet<String>() {
         {
@@ -37,60 +33,76 @@ public final class Pack {
             // Logback
             add("ch.qos");
             add("org.slf4j");
+            // Vert.x
+            add("io.vertx.core");
+            add("io.vertx.spi");
             // Asm
             add("org.ow2");
             add("org.objectweb");
             add("com.esotericsoftware");
             // Hazelcast
             add("com.hazelcast");
-            // Vert.x core
-            add("io.vertx.core");
-            add("io.vertx.spi");
+            // Glassfish
+            add("org.glassfish");
         }
     };
 
     public static Set<Class<?>> getClasses(final Predicate<Class<?>> filter,
                                            final String... zeroScans) {
-        if (CLASSES.isEmpty()) {
-            if (0 < zeroScans.length) {
-                CLASSES.addAll(collectClasses(zeroScans, filter));
-            } else {
-                final Package[] packages = Package.getPackages();
-                final Set<String> packageDirs = new HashSet<>();
-                for (final Package pkg : packages) {
-                    final String pending = pkg.getName();
-                    final boolean skip = FORBIDDEN.stream().anyMatch(pending::startsWith);
-                    if (!skip) {
-                        packageDirs.add(pending);
-                    }
+        final Set<Class<?>> classes = new HashSet<>();
+        if (0 < zeroScans.length) {
+            classes.addAll(multiClasses(zeroScans, filter));
+        } else {
+            final Package[] packages = Package.getPackages();
+            final Set<String> packageDirs = new HashSet<>();
+            for (final Package pkg : packages) {
+                final String pending = pkg.getName();
+                final boolean skip = FORBIDDEN.stream().anyMatch(pending::startsWith);
+                if (!skip) {
+                    packageDirs.add(pending);
                 }
-                LOGGER.info(Info.PACKAGES, String.valueOf(packageDirs.size()),
-                        String.valueOf(packages.length));
-                CLASSES.addAll(collectClasses(packageDirs.toArray(new String[]{}), filter));
             }
+            LOGGER.info(Info.PACKAGES, String.valueOf(packageDirs.size()),
+                    String.valueOf(packages.length));
+            classes.addAll(multiClasses(packageDirs.toArray(new String[]{}), filter));
         }
-        return CLASSES;
+        return classes;
     }
 
-    private static Set<Class<?>> collectClasses(final String[] packageDir, final Predicate<Class<?>> filter) {
+    private static Set<Class<?>> singleClasses(
+            final String[] packageDir,
+            final Predicate<Class<?>> filter) {
+        final Set<Class<?>> result = new HashSet<>();
+        for (final String pkgName : packageDir) {
+            result.addAll(PackScanner.getClasses(filter, pkgName));
+        }
+        return result;
+    }
+
+    private static Set<Class<?>> multiClasses(
+            final String[] packageDir,
+            final Predicate<Class<?>> filter) {
         // Counter
-        final CountDownLatch counter = new CountDownLatch(packageDir.length);
         final Set<PackThread> references = new HashSet<>();
         for (final String pkgName : packageDir) {
-            final PackThread thread = new PackThread(pkgName, filter, counter);
+            final PackThread thread = new PackThread(pkgName, filter);
             references.add(thread);
             thread.start();
         }
         // Wait
         final Set<Class<?>> result = new HashSet<>();
         try {
-            counter.await();
+            for (final PackThread item : references) {
+                item.join();
+            }
             // Collect all the classes
             for (final PackThread thread : references) {
                 result.addAll(thread.getClasses());
             }
         } catch (final InterruptedException ex) {
             LOGGER.jvm(ex);
+        } catch (final Throwable ex) {
+            ex.printStackTrace();
         }
         return result;
     }
