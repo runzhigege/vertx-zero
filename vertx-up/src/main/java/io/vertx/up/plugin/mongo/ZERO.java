b35c691385d3cf05f8e526598bad3b0c5fab9755
Lang <silentbalanceyh@126.com>
2017-12-17 22:34:24 +0800

    Update successfully

--- vertx-up/src/main/java/io/vertx/up/plugin/mongo/ZERO.java
+++ vertx-up/src/main/java/io/vertx/up/plugin/mongo/ZERO.java
@@ -11,4 +11,6 @@ interface Info {
     String UPDATE_QUERY = "[ ZERO ] Mongo collection = {0}, query by: {1}, data: {2}";
 
     String UPDATE_FLOW = "[ ZERO ] Mongo update flow = {0}, filter: {1}, latest: {2}";
+
+    String UPDATE_RESULT = "[ ZERO ] Mongo update result = {0}.";
 }
