506e1b75179494c331ae35929c1add8a91b419d7
Lang <silentbalanceyh@126.com>
2018-01-03 16:19:12 +0800

    Authorization Handler finished

--- vertx-zeus/up-apollo/src/main/java/up/god/apollo/exp4/MongoKeeper.java
+++ vertx-zeus/up-apollo/src/main/java/up/god/apollo/exp4/MongoKeeper.java
@@ -1,11 +1,9 @@
 package up.god.apollo.exp4;
 
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.mongo.MongoAuth;
 import io.vertx.ext.web.handler.AuthHandler;
 import io.vertx.up.annotations.Authenticate;
-import io.vertx.up.annotations.Authorize;
 import io.vertx.up.annotations.Wall;
 import io.vertx.up.plugin.mongo.MongoInfix;
 import io.vertx.up.secure.handler.BasicOstium;
@@ -18,11 +16,5 @@ public class MongoKeeper {
         return BasicOstium.create(
                 MongoAuth.create(MongoInfix.getClient(), config)
         );
-    }
-
-    @Authorize
-    public boolean authorize(final User user) {
-        System.out.println(user);
-        return true;
     }
 }
