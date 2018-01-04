4b196e7b515c88fe2b74f21d2806a3bf9a1cc0fe
Lang <silentbalanceyh@126.com>
2018-01-04 18:52:43 +0800

    JooqMySQL switch to 5.x

--- vertx-tp/src/main/java/io/vertx/tp/plugin/jooq/JooqInfix.java
+++ vertx-tp/src/main/java/io/vertx/tp/plugin/jooq/JooqInfix.java
@@ -1,7 +1,6 @@
 package io.vertx.tp.plugin.jooq;
 
 import io.github.jklingsporn.vertx.jooq.async.future.AsyncJooqSQLClient;
-import io.github.jklingsporn.vertx.jooq.async.future.VertxDAO;
 import io.vertx.core.Vertx;
 import io.vertx.ext.asyncsql.MySQLClient;
 import io.vertx.up.annotations.Plugin;
@@ -58,11 +57,11 @@ public class JooqInfix implements Infix {
         return CLIENTS.get(NAME);
     }
 
-    public static <T extends VertxDAO> T getDao(final Class<T> clazz) {
+    public static <T> T getDao(final Class<T> clazz) {
         final Object reference = Fn.pool(DAOS, clazz.getName(), () -> {
             final Configuration configuration = CONFIGS.get(clazz.getName());
-            final VertxDAO dao = Instance.instance(clazz, configuration);
-            dao.setClient(getClient());
+            final Object dao = Instance.instance(clazz, configuration);
+            Instance.invoke(dao, "setClient", getClient());
             return dao;
         });
         return (T) reference;
