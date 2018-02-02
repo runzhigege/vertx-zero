4a3728aef93229086a15cd76d305db4042d91c83
Lang <silentbalanceyh@126.com>
2018-02-02 21:31:55 +0800

    Fix00004: Fix Set/HashSet/TreeSet serialization

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/web/serialization/CollectionSaber.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/web/serialization/CollectionSaber.java
@@ -1,13 +1,8 @@
 package io.vertx.up.web.serialization;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import io.vertx.core.json.JsonArray;
 import io.vertx.up.func.Fn;
 import io.vertx.up.tool.Jackson;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Enum
@@ -16,22 +11,15 @@ public class CollectionSaber extends BaseSaber {
     @Override
     public <T> Object from(final T input) {
         return Fn.get(() -> {
-            Object reference = null;
-            if (input instanceof Collection) {
-                if (input.getClass().isArray()) {
-                    final String literal = Jackson.serialize(input);
-                    reference = new JsonArray(literal);
-                } else if (input instanceof List) {
-                    // List
-                    reference = Jackson.deserialize(input.toString(), new TypeReference<List>() {
-                    });
-                } else if (input instanceof Set) {
-                    // Set
-                    reference = Jackson.deserialize(input.toString(), new TypeReference<Set>() {
-                    });
-                }
-            }
-            return reference;
+            final String literal = Jackson.serialize(input);
+            return new JsonArray(literal);
         }, input);
+    }
+
+    @Override
+    public Object from(final Class<?> paramType,
+                       final String literal) {
+        // Default direct
+        return Jackson.deserialize(literal, paramType);
     }
 }
