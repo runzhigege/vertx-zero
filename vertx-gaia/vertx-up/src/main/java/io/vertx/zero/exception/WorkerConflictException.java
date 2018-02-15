83e2880717f8368ba005607a446c046fb160a1e1
Lang <silentbalanceyh@126.com>
2018-02-15 20:55:32 +0800

    Method Conflicts Exception

---
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/zero/exception/WorkerConflictException.java
@@ -0,0 +1,16 @@
+package io.vertx.zero.exception;
+
+import java.lang.reflect.Method;
+
+public class WorkerConflictException extends UpException {
+
+    public WorkerConflictException(final Class<?> clazz,
+                                   final Method method) {
+        super(clazz, method.getName(), method.getDeclaringClass());
+    }
+
+    @Override
+    public int getCode() {
+        return -40049;
+    }
+}
