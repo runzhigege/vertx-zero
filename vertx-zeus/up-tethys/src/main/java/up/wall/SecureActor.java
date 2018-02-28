78e19274876d341dc4fd9613bc84cab844578b1b
silentbalanceyh <silentbalanceyh@126.com>
2018-02-28 19:01:02 +0800

    Add Jwt Handler

--- vertx-zeus/up-tethys/src/main/java/up/wall/SecureActor.java
+++ vertx-zeus/up-tethys/src/main/java/up/wall/SecureActor.java
@@ -1,20 +1,21 @@
 package up.wall;
 
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.mongo.MongoAuth;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.ext.web.handler.AuthHandler;
 import io.vertx.up.annotations.Authenticate;
 import io.vertx.up.annotations.Wall;
-import io.vertx.up.plugin.mongo.MongoInfix;
-import io.vertx.up.secure.handler.BasicOstium;
+import io.vertx.up.secure.handler.JwtOstium;
 
-@Wall(value = "mongo", path = "/api/*")
+@Wall(value = "jwt", path = "/api/*")
 public class SecureActor {
     @Authenticate
-    public AuthHandler authenticate(final JsonObject config) {
-        return BasicOstium.create(
-                MongoAuth.create(MongoInfix.getClient(), config)
+    public AuthHandler authenticate(final Vertx vertx,
+                                    final JsonObject config) {
+        return JwtOstium.create(
+                JWTAuth.create(vertx, new JWTAuthOptions(config))
         );
     }
-
 }
