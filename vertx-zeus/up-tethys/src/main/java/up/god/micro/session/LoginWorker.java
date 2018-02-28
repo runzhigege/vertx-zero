c8642737af4a2864d3613ef0d0146bce02ff6ce9
silentbalanceyh <silentbalanceyh@126.com>
2018-03-01 06:31:13 +0800

    Add new project for future usage

--- vertx-zeus/up-tethys/src/main/java/up/god/micro/session/LoginWorker.java
+++ vertx-zeus/up-tethys/src/main/java/up/god/micro/session/LoginWorker.java
@@ -2,14 +2,12 @@ package up.god.micro.session;
 
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
-import io.vertx.up.annotations.Address;
 import io.vertx.up.annotations.Queue;
 import io.vertx.up.atom.Envelop;
 
 @Queue
 public class LoginWorker {
 
-    @Address("ZERO://SECURE/MSG")
     public Future<JsonObject> secure(final Envelop envelop) {
         return Future.succeededFuture(new JsonObject().put("_id", "Lang"));
     }
