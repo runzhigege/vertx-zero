61cc7c2a2de71a73fbc3c34a13cbb52eabbb10b0
silentbalanceyh <silentbalanceyh@126.com>
2018-03-10 23:32:21 +0800

    Plugin Object Reference finished

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/web/anima/AffluxPlugin.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/web/anima/AffluxPlugin.java
@@ -1,7 +1,18 @@
 package io.vertx.up.web.anima;
 
+import io.reactivex.Observable;
+import io.vertx.up.annotations.Plugin;
 import io.vertx.up.func.Fn;
 import io.vertx.up.log.Annal;
+import io.vertx.up.plugin.Infix;
+import io.vertx.up.tool.mirror.Instance;
+import io.vertx.up.web.ZeroAmbient;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 class AffluxPlugin {
 
@@ -18,6 +29,39 @@ class AffluxPlugin {
     }
 
     void inject(final Object proxy) {
-        
+        final ConcurrentMap<Class<?>, Class<?>> binds = this.getBind();
+        final Class<?> type = proxy.getClass();
+        Observable.fromArray(type.getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(Plugin.class))
+                .subscribe(field -> {
+                    final Class<?> fieldType = field.getType();
+                    final Class<?> infixCls = binds.get(fieldType);
+                    if (null != infixCls) {
+                        if (Instance.isMatch(infixCls, Infix.class)) {
+                            final Infix reference = Instance.singleton(infixCls);
+                            final Object tpRef = Instance.invoke(reference, "get");
+                            final String fieldName = field.getName();
+                            Instance.set(proxy, fieldName, tpRef);
+                        } else {
+                            this.logger.warn(Info.INFIX_IMPL, infixCls.getName(), Infix.class.getName());
+                        }
+                    } else {
+                        this.logger.warn(Info.INFIX_NULL, "tp", field.getName(), type.getName());
+                    }
+                });
+    }
+
+    private ConcurrentMap<Class<?>, Class<?>> getBind() {
+        // Extract all infixes
+        final Set<Class<?>> infixes = new HashSet<>(ZeroAmbient.getInjections().values());
+        final ConcurrentMap<Class<?>, Class<?>> binds = new ConcurrentHashMap<>();
+        Observable.fromIterable(infixes)
+                .filter(Infix.class::isAssignableFrom)
+                .subscribe(item -> {
+                    final Method method = Fn.getJvm(() -> item.getDeclaredMethod("get"), item);
+                    final Class<?> type = method.getReturnType();
+                    binds.put(type, item);
+                });
+        return binds;
     }
 }
