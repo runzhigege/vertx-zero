f4609aa43455f088edf7f6f2adb4447ca111b0c9
Lang <silentbalanceyh@126.com>
2018-07-23 08:16:07 +0800

    2018-07-29T16:26:49格式的解析添加, yyyy-MM-dd'T'HH:mm:ss

--- vertx-gaia/vertx-co/src/test/java/io/vertx/up/tool/PeriodTc.java
+++ vertx-gaia/vertx-co/src/test/java/io/vertx/up/tool/PeriodTc.java
@@ -2,16 +2,21 @@ package io.vertx.up.tool;
 
 import org.junit.Test;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public class PeriodTc {
 
     @Test
     public void valueDurationDays() {
-        List<String> result = Period.valueDurationDays(Ut.toDateTime("2018-04-02T06:49:41.661Z").toLocalDate().toString(),Ut.toDateTime("2018-05-01T06:49:39.096Z").toLocalDate().toString());
+        final List<String> result = Period.valueDurationDays(Ut.toDateTime("2018-04-02T06:49:41.661Z").toLocalDate().toString(), Ut.toDateTime("2018-05-01T06:49:39.096Z").toLocalDate().toString());
         System.out.println(result);
         System.out.println(Ut.toDate(result.get(0)));
+    }
+
+    @Test
+    public void valueTest() {
+        final Date date = Ut.parse("2018-07-29T16:26:49");
+        System.out.println(date);
     }
 }
