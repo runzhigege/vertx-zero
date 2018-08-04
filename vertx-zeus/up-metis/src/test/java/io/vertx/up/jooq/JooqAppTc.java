5d9802c36749d6f66f52ac4248f8a33a57ef41cf
Lang <silentbalanceyh@126.com>
2018-08-04 16:46:22 +0800

    Add naming strategy for keep the old name of Pojo

--- vertx-zeus/up-metis/src/test/java/io/vertx/up/jooq/JooqAppTc.java
+++ vertx-zeus/up-metis/src/test/java/io/vertx/up/jooq/JooqAppTc.java
@@ -2,11 +2,17 @@ package io.vertx.up.jooq;
 
 import com.htl.cv.Pojo;
 import com.htl.domain.tables.daos.SysAppDao;
+import com.htl.domain.tables.pojos.SysApp;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.tp.plugin.jooq.JooqInfix;
 import io.vertx.up.aiki.Ux;
 import io.vertx.up.aiki.UxJooq;
+import io.zero.epic.Ut;
 import io.zero.quiz.JooqBase;
+import org.junit.Test;
+
+import java.util.UUID;
 
 public class JooqAppTc extends JooqBase {
 
@@ -50,5 +56,19 @@ public class JooqAppTc extends JooqBase {
                 "fetchOneAsync1.json",
                 "fetchOneAsync2.json",
                 "fetchOneAsync3.json");
+    }
+
+    @Test
+    public void testFetchFields() {
+        final SysApp app = new SysApp();
+        app.setPkId(UUID.randomUUID().toString());
+        app.setSName("Lang");
+        app.setSCode("LANG.APP");
+        app.setIsActive(Boolean.TRUE);
+        app.setIAttachSize(12);
+        final JsonObject json = Ut.serializeJson(app);
+        System.out.println(json.encodePrettily());
+        final SysApp target = Ut.deserialize(json, SysApp.class);
+        System.out.println(target);
     }
 }
