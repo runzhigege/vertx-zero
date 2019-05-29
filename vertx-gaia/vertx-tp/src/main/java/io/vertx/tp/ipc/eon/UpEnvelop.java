ab57bc6bff2cbc2b9ab80e654b9fc681a126eea9
Lang <silentbalanceyh@126.com>
2019-05-30 06:30:00 +0800

    Upgrade Core Library

--- vertx-gaia/vertx-tp/src/main/java/io/vertx/tp/ipc/eon/UpEnvelop.java
+++ vertx-gaia/vertx-tp/src/main/java/io/vertx/tp/ipc/eon/UpEnvelop.java
@@ -63,20 +63,12 @@ public final class UpEnvelop {
       "B\"\n\023io.vertx.tp.ipc.eonB\tUpEnvelopP\001b\006pr" +
       "oto3"
     };
-    com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
-        new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
-          public com.google.protobuf.ExtensionRegistry assignDescriptors(
-              com.google.protobuf.Descriptors.FileDescriptor root) {
-            descriptor = root;
-            return null;
-          }
-        };
-    com.google.protobuf.Descriptors.FileDescriptor
+    descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
         new com.google.protobuf.Descriptors.FileDescriptor[] {
           io.vertx.tp.ipc.eon.em.UpEnum.getDescriptor(),
           io.vertx.tp.ipc.eon.UpStatus.getDescriptor(),
-        }, assigner);
+        });
     internal_static_io_vertx_tp_ipc_eon_IpcContent_descriptor =
       getDescriptor().getMessageTypes().get(0);
     internal_static_io_vertx_tp_ipc_eon_IpcContent_fieldAccessorTable = new
