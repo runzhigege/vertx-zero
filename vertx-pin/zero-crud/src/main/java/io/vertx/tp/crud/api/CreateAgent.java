7eb9a5f694c007b9fa1273ec31f512e25b845176
Lang <silentbalanceyh@126.com>
2019-06-01 09:41:58 +0800

    New Crud module

---
+++ vertx-pin/zero-crud/src/main/java/io/vertx/tp/crud/api/CreateAgent.java
@@ -0,0 +1,25 @@
+package io.vertx.tp.crud.api;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.tp.crud.cv.Addr;
+import io.vertx.up.annotations.Address;
+import io.vertx.up.annotations.Codex;
+import io.vertx.up.annotations.EndPoint;
+
+import javax.ws.rs.BodyParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/*
+ * HTTP Method: POST
+ */
+@EndPoint
+@Path("/api")
+public interface CreateAgent {
+    @POST
+    @Path("/{actor}")
+    @Address(Addr.Post.ADD)
+    JsonObject create(@PathParam("actor") String actor,
+                      @BodyParam @Codex JsonObject data);
+}
