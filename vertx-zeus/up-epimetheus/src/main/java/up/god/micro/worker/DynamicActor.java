bb9932ff01cc31db886fa86376b0e26a2e4fa487
Lang <silentbalanceyh@126.com>
2019-06-11 14:11:47 +0800

    Split Example

--- vertx-zeus/up-epimetheus/src/main/java/up/god/micro/worker/DynamicActor.java
+++
@@ -1,8 +0,0 @@
-package up.god.micro.worker;
-
-public class DynamicActor implements DynamicApi {
-    @Override
-    public String sayEnvelop(final String name) {
-        return name;
-    }
-}
--- vertx-zeus/zk8s-ns.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-kubectl create -f init/zero-k8s-namespace.yaml
