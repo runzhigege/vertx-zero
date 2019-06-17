b164573f21936259c6eeb33ababf9ad4ff9d1361
Lang <silentbalanceyh@126.com>
2019-06-17 10:31:24 +0800

    Filters finished

--- vertx-pin/zero-rbac/src/main/java/io/vertx/tp/rbac/extension/dwarf/ArrayDwarf.java
+++ vertx-pin/zero-rbac/src/main/java/io/vertx/tp/rbac/extension/dwarf/ArrayDwarf.java
@@ -1,10 +1,25 @@
 package io.vertx.tp.rbac.extension.dwarf;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
+/*
+ * rows only
+ */
 class ArrayDwarf implements DataDwarf {
+    /*
+     * {
+     *     "data": []
+     * }
+     *
+     */
     @Override
     public void minimize(final JsonObject dataReference, final JsonObject matrix) {
-        System.out.println(matrix.encodePrettily());
+        /* inputArray */
+        final JsonArray inputArray = dataReference.getJsonArray("data");
+        /* rows */
+        final JsonArray updated = Dwarf.onRows(inputArray, matrix.getJsonObject("rows"));
+        /* Updated */
+        dataReference.put("data", updated);
     }
 }
