a735aceea2938804f98e8d93513e3f172110586c
Lang <silentbalanceyh@126.com>
2019-06-24 11:32:32 +0800

    View stored into ScRequest to do split

--- vertx-pin/zero-ui/src/main/java/io/vertx/tp/ui/cv/UiMsg.java
+++ vertx-pin/zero-ui/src/main/java/io/vertx/tp/ui/cv/UiMsg.java
@@ -3,4 +3,6 @@ package io.vertx.tp.ui.cv;
 public interface UiMsg {
 
     String COLUMN_STATIC = "Column data extracted with sigma = `{0}` and size = `{1}`. Data = {2}";
+
+    String COLUMN_FULL = "Full Column params : \n{0}";
 }
