5a5b8827a46b75f06e09ea8bd6b05c5833aed659
Lang <silentbalanceyh@126.com>
2019-06-27 15:42:51 +0800

    Set worker instance configuration

---
+++ vertx-pin/zero-jet/src/main/java/io/vertx/tp/jet/init/JtPin.java
@@ -0,0 +1,12 @@
+package io.vertx.tp.jet.init;
+
+import io.vertx.tp.ke.tool.Ke;
+import io.vertx.up.log.Annal;
+
+public class JtPin {
+    private static final Annal LOGGER = Annal.get(JtPin.class);
+
+    public static void init() {
+        Ke.banner("「Πίδακας δρομολογητή」- ( Api )");
+    }
+}
---
+++ vertx-pin/zero-jet/src/main/java/io/vertx/tp/jet/refine/Jt.java
@@ -0,0 +1,10 @@
+package io.vertx.tp.jet.refine;
+
+import io.vertx.up.log.Annal;
+
+public class Jt {
+
+    public static void infoInit(final Annal logger, final String pattern, final Object... args) {
+        JtLog.infoInit(logger, pattern, args);
+    }
+}
