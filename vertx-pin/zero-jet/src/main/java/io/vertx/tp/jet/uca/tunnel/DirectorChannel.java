1157fbaf869662b76c56d5c33d2cb827ad68dc7b
Lang <silentbalanceyh@126.com>
2019-07-07 17:31:35 +0800

    Add specification

--- vertx-pin/zero-jet/src/main/java/io/vertx/tp/jet/uca/tunnel/DirectorChannel.java
+++ vertx-pin/zero-jet/src/main/java/io/vertx/tp/jet/uca/tunnel/DirectorChannel.java
@@ -2,12 +2,32 @@ package io.vertx.tp.jet.uca.tunnel;
 
 import io.vertx.core.Future;
 import io.vertx.tp.optic.jet.JtComponent;
+import io.vertx.up.aiki.Ux;
+import io.vertx.up.atom.worker.Mission;
 import io.vertx.up.commune.ActIn;
+import io.vertx.zero.atom.Database;
+import io.zero.epic.Ut;
 
 public class DirectorChannel extends AbstractChannel {
+    /*
+     * Adaptor is pure to access dynamic database here, the specification is as:
+     * Step 1:
+     * - The component defined Database reference, it could be initialized
+     * Step 2:
+     * - The component defined Mission reference, it could be initialized
+     */
     @Override
     public Future<Boolean> initAsync(final JtComponent component, final ActIn request) {
-        // TODO:
-        return Future.succeededFuture(Boolean.TRUE);
+        return Ux.toFuture(this.getCommercial())
+                /*
+                 * Database initialized
+                 */
+                .compose(Anagogic::databaseAsync)
+                .compose(database -> Ut.contractAsync(component, Database.class, database))
+                /*
+                 * Mission inited, mount to `JtComponent`
+                 */
+                .compose(dbed -> Ux.toFuture(this.getMission()))
+                .compose(mission -> Ut.contractAsync(component, Mission.class, mission));
     }
 }
