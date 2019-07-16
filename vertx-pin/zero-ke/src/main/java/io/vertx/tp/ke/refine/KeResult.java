1389f7e8f53856bd3cc6f5058aa36830a82e2137
Lang <silentbalanceyh@126.com>
2019-07-16 11:29:51 +0800

    Routing of Micro

--- vertx-pin/zero-ke/src/main/java/io/vertx/tp/ke/refine/KeResult.java
+++ vertx-pin/zero-ke/src/main/java/io/vertx/tp/ke/refine/KeResult.java
@@ -4,6 +4,7 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.tp.ke.cv.KeField;
 import io.vertx.tp.ke.cv.WebResult;
+import io.vertx.up.util.Ut;
 
 class KeResult {
 
@@ -11,6 +12,12 @@ class KeResult {
         final WebResult.Bool response = checked ?
                 WebResult.Bool.SUCCESS : WebResult.Bool.FAILURE;
         return new JsonObject().put(key, response.name());
+    }
+
+    static Boolean bool(final JsonObject checkedJson) {
+        final String result = checkedJson.getString(KeField.RESULT);
+        final WebResult.Bool resultValue = Ut.toEnum(() -> result, WebResult.Bool.class, WebResult.Bool.FAILURE);
+        return WebResult.Bool.SUCCESS == resultValue;
     }
 
     static JsonObject array(final JsonArray array) {
