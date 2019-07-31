7989507ad7f084659663b39b088b108fc09361d3
Lang <silentbalanceyh@126.com>
2019-07-31 15:20:05 +0800

    Region/Habitus

--- vertx-gaia/vertx-co/src/main/java/io/vertx/up/unity/Uson.java
+++ vertx-gaia/vertx-co/src/main/java/io/vertx/up/unity/Uson.java
@@ -3,9 +3,9 @@ package io.vertx.up.unity;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.up.fn.Fn;
 import io.vertx.up.log.Annal;
 import io.vertx.up.util.Ut;
-import io.vertx.up.fn.Fn;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -20,8 +20,8 @@ public class Uson {
     private final transient JsonObject objectReference;
 
     private Uson(final JsonObject json) {
-        this.objectReference = Fn.getNull(new JsonObject(), () -> json, json);
-        LOGGER.debug(StreamInfo.STREAM_START, String.valueOf(this.hashCode()), json);
+        objectReference = Fn.getNull(new JsonObject(), () -> json, json);
+        LOGGER.debug(StreamInfo.STREAM_START, String.valueOf(hashCode()), json);
     }
 
     public static Uson create(final String field, final Object value) {
@@ -37,86 +37,86 @@ public class Uson {
     }
 
     public Uson append(final JsonObject object) {
-        Dual.append(this.objectReference, object, false);
+        Dual.append(objectReference, object, false);
         return this;
     }
 
     public Uson append(final JsonArray array) {
-        Dual.append(this.objectReference, array);
+        Dual.append(objectReference, array);
         return this;
     }
 
     public Uson append(final String field, final Object value) {
-        this.objectReference.put(field, value);
+        objectReference.put(field, value);
         return this;
     }
 
     public Uson convert(final String from, final String to) {
-        Self.convert(this.objectReference, new ConcurrentHashMap<String, String>() {{
-            this.put(from, to);
+        Self.convert(objectReference, new ConcurrentHashMap<String, String>() {{
+            put(from, to);
         }}, false);
         return this;
     }
 
     public Uson dft(final String field, final Object value) {
-        Self.defaultValue(this.objectReference, field, value, false);
+        Self.defaultValue(objectReference, field, value, false);
         return this;
     }
 
     public Uson dft(final JsonObject values) {
-        Self.defaultValue(this.objectReference, values, false);
+        Self.defaultValue(objectReference, values, false);
         return this;
     }
 
     public Uson plus(final String from, final Integer seed) {
-        final Object value = this.objectReference.getValue(from);
+        final Object value = objectReference.getValue(from);
         if (null != value && Ut.isInteger(value)) {
-            final Integer old = this.objectReference.getInteger(from);
-            this.objectReference.put(from, old + seed);
+            final Integer old = objectReference.getInteger(from);
+            objectReference.put(from, old + seed);
         }
         return this;
     }
 
     public <I, O> Uson convert(final String field, final Function<I, O> function) {
-        Self.convert(this.objectReference, field, function, false);
+        Self.convert(objectReference, field, function, false);
         return this;
     }
 
     public Uson copy(final String from, final String to) {
-        Self.copy(this.objectReference, from, to, false);
+        Self.copy(objectReference, from, to, false);
         return this;
     }
 
     public Uson remove(final String... keys) {
-        Self.remove(this.objectReference, false, keys);
+        Self.remove(objectReference, false, keys);
         return this;
     }
 
     public Uson pickup(final String... keys) {
-        Self.pickup(this.objectReference, keys);
+        Self.pickup(objectReference, keys);
         return this;
     }
 
     public Uson denull() {
-        Self.deNull(this.objectReference, false);
+        Self.deNull(objectReference, false);
         return this;
     }
 
     public JsonObject to() {
-        LOGGER.debug(StreamInfo.STREAM_END, String.valueOf(this.hashCode()), this.objectReference);
-        return this.objectReference;
+        LOGGER.debug(StreamInfo.STREAM_END, String.valueOf(hashCode()), objectReference);
+        return objectReference;
     }
 
     public Future<JsonObject> toFuture() {
-        return Future.succeededFuture(this.to());
+        return Future.succeededFuture(to());
     }
 
     public Object get(final String field) {
-        return this.objectReference.getValue(field);
+        return objectReference.getValue(field);
     }
 
     @Override
     public String toString() {
-        return this.objectReference.encode();
+        return objectReference.encode();
     }
 }
--- vertx-pin/zero-rbac/src/main/java/io/vertx/tp/optic/ExIntimitySeeker.java
+++ vertx-pin/zero-rbac/src/main/java/io/vertx/tp/optic/ExIntimitySeeker.java
@@ -5,12 +5,15 @@ import cn.vertxup.rbac.service.accredit.ActionStub;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
+import io.vertx.tp.error._403ActionMissingException;
 import io.vertx.tp.ke.cv.KeField;
 import io.vertx.tp.optic.fantom.Anchoret;
 import io.vertx.tp.rbac.cv.AuthMsg;
 import io.vertx.tp.rbac.refine.Sc;
 import io.vertx.up.unity.Uson;
 import io.vertx.up.util.Ut;
+
+import java.util.Objects;
 
 /*
  * Seek impact resource for params here, it will be passed by crud
@@ -28,9 +31,9 @@ public class ExIntimitySeeker extends Anchoret<Seeker> implements Seeker {
         final HttpMethod method = HttpMethod.valueOf(params.getString(ARG1));
         final String sigma = params.getString(ARG2);
         Sc.infoResource(getLogger(), AuthMsg.SEEKER_RESOURCE, uri, method, sigma);
-        return stub.fetchAction(uri, method, sigma)
-                .compose(action -> Uson.create(params)
-                        .append(KeField.RESOURCE_ID, action.getResourceId())
+        return stub.fetchAction(uri, method, sigma).compose(action -> Objects.isNull(action) ?
+                Future.failedFuture(new _403ActionMissingException(getClass(), method + " " + uri)) :
+                Uson.create(params).append(KeField.RESOURCE_ID, action.getResourceId())
                         .toFuture());
     }
 }
