663be273aea648f9076f6805bbb9956c1eaddcff
Lang <silentbalanceyh@126.com>
2019-08-28 16:29:57 +0800

    Page/Layout

---
+++ vertx-pin/zero-ui/src/main/java/io/vertx/tp/ui/service/PageService.java
@@ -0,0 +1,53 @@
+package io.vertx.tp.ui.service;
+
+import cn.vertxup.ui.domain.tables.daos.UiPageDao;
+import cn.vertxup.ui.domain.tables.pojos.UiPage;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.tp.ke.cv.KeField;
+import io.vertx.up.unity.Ux;
+import io.vertx.up.util.Ut;
+
+import javax.inject.Inject;
+import java.util.Objects;
+
+public class PageService implements PageStub {
+    @Inject
+    private transient LayoutStub layoutStub;
+
+    @Override
+    public Future<JsonObject> fetchAmp(final String sigma,
+                                       final JsonObject params) {
+        final JsonObject filters = params.copy();
+        filters.put(KeField.SIGMA, sigma);
+        filters.put("", Boolean.TRUE);
+        return Ux.Jooq.on(UiPageDao.class)
+                .<UiPage>fetchOneAsync(filters)
+                .compose(page -> {
+                    if (Objects.nonNull(page)) {
+                        /*
+                         * Page Existing in current system
+                         */
+                        if (Ut.notNil(page.getLayoutId())) {
+                            /*
+                             * Continue to extract layout Data here
+                             */
+                            return this.layoutStub.fetchLayout(page.getLayoutId())
+                                    .compose(Ux::fnJObject)
+                                    .compose(layout -> {
+                                        final JsonObject pageJson = Ux.toJson(page);
+                                        pageJson.put("layout", layout);
+                                        return Ux.toFuture(pageJson);
+                                    });
+                        } else {
+                            return Ux.fnJObject(page);
+                        }
+                    } else {
+                        /*
+                         * No configuration related to current page
+                         */
+                        return Ux.toFuture(new JsonObject());
+                    }
+                });
+    }
+}
