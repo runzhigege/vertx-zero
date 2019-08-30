b71f292f7327ca6c3d9f799c67de8b74a3c9f3e0
zwkjhx <1264210109@qq.com>
2019-08-30 17:11:06 +0800

    公司及部门查询接口添加

---
+++ vertx-pin/zero-erp/src/main/java/cn/vertxup/erp/api/DeptActor.java
@@ -0,0 +1,28 @@
+package cn.vertxup.erp.api;
+
+import io.vertx.tp.erp.cv.ErpMsg;
+import io.vertx.tp.erp.refine.Er;
+import io.vertx.tp.erp.service.DeptStub;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
+import io.vertx.tp.erp.cv.Addr;
+import io.vertx.up.annotations.Address;
+import io.vertx.up.annotations.Queue;
+import io.vertx.up.log.Annal;
+
+import javax.inject.Inject;
+
+@Queue
+public class DeptActor {
+
+    private static final Annal LOGGER = Annal.get(DeptActor.class);
+
+    @Inject
+    private transient DeptStub deptStub;
+
+    @Address(Addr.Dept.DEPT_KEY)
+    public Future<JsonArray> fetchDepts(final String sigma) {
+        Er.infoWorker(LOGGER, ErpMsg.DEPT_INFO, sigma);
+        return deptStub.fetchDepts(sigma);
+    }
+}
