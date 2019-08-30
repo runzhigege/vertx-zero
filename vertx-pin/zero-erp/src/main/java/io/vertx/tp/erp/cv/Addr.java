d8f36518f787dd7f327a9108b5a4800d686e22f7
zwkjhx <1264210109@qq.com>
2019-08-30 17:13:06 +0800

    公司及部门查询接口添加

--- vertx-pin/zero-erp/src/main/java/io/vertx/tp/erp/cv/Addr.java
+++ vertx-pin/zero-erp/src/main/java/io/vertx/tp/erp/cv/Addr.java
@@ -9,5 +9,9 @@ public interface Addr {
 
     interface Company {
         String INFORMATION = Prefix._EVENT + "X-COMPANY";
+        String COMPANY_KEY = Prefix._EVENT + "E-COMPANY/KEY";
+    }
+    interface Dept {
+        String DEPT_KEY = Prefix._EVENT + "E-DEPT/KEY";
     }
 }
