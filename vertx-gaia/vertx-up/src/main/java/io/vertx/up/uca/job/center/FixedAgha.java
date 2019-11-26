a4193d46b54a7281a8cf2963e62e6a6dd3913a43
Lang <silentbalanceyh@126.com>
2019-11-27 00:31:13 +0800

    JobPool Timer here

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/job/center/FixedAgha.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/job/center/FixedAgha.java
@@ -2,6 +2,7 @@ package io.vertx.up.uca.job.center;
 
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.tp.plugin.job.JobPool;
 import io.vertx.up.atom.worker.Mission;
 import io.vertx.up.eon.Info;
 import io.vertx.up.util.Ut;
@@ -33,6 +34,7 @@ class FixedAgha extends AbstractAgha {
              */
             Ut.itRepeat(2, () -> this.moveOn(mission, true));
         }));
+        JobPool.mount(jobId, mission.getCode());
         this.getLogger().info(Info.JOB_INTERVAL, mission.getCode(),
                 String.valueOf(delay), String.valueOf(mission.getDuration()), String.valueOf(jobId));
         return future.future();
