c7f0d4216ca79f9fee7c3cb6220c1672170ca18b
Lang <silentbalanceyh@126.com>
2019-11-29 22:21:07 +0800

    BtLoader recovery

--- vertx-pin/zero-ke/src/main/java/io/vertx/tp/ke/booter/Bt.java
+++ vertx-pin/zero-ke/src/main/java/io/vertx/tp/ke/booter/Bt.java
@@ -1,7 +1,9 @@
 package io.vertx.tp.ke.booter;
 
-import io.vertx.core.*;
-import io.vertx.core.json.JsonObject;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.WorkerExecutor;
 import io.vertx.tp.plugin.excel.atom.ExTable;
 
 import java.util.List;
@@ -26,22 +28,18 @@ public class Bt {
      * doImport: with prefix to do filter
      */
     public static void doImport(final String folder) {
-        BtLoader.doImport(folder);
+        BtLoader.importSyncs(folder);
     }
 
     public static void doImport(final String folder, final String prefix) {
-        BtLoader.doImport(folder, prefix);
+        BtLoader.importSyncs(folder, prefix);
     }
 
     public static void doImport(final String folder, final Handler<AsyncResult<List<String>>> callback) {
-        BtLoader.doImport(folder, null, callback);
-    }
-
-    public static Future<JsonObject> asyncImport(final String filename) {
-        return BtLoader.asyncImport(filename);
+        BtLoader.importSyncs(folder, callback);
     }
 
     public static void ingestExcels(final String folder, final Handler<AsyncResult<Set<ExTable>>> callback) {
-
+        BtLoader.ingestExcels(folder, callback);
     }
 }
--- vertx-pin/zero-ke/src/main/java/io/vertx/tp/ke/refine/KeRun.java
+++ vertx-pin/zero-ke/src/main/java/io/vertx/tp/ke/refine/KeRun.java
@@ -8,13 +8,14 @@ import java.util.Objects;
 import java.util.function.Function;
 
 class KeRun {
-
     static <T, O> Future<O> channel(final Class<T> clazz, final O defaultValue,
                                     final Function<T, Future<O>> executor) {
         final T channel = Pocket.lookup(clazz);
         if (Objects.isNull(channel)) {
+            KeLog.warnChannel(KeRun.class, "Channel {0} null", clazz.getName());
             return Ux.future(defaultValue);
         } else {
+            KeLog.infoChannel(KeRun.class, "Channel Async selected {0}", channel.getClass().getName());
             return executor.apply(channel);
         }
     }
@@ -23,8 +24,10 @@ class KeRun {
                                 final Function<T, O> executor) {
         final T channel = Pocket.lookup(clazz);
         if (Objects.isNull(channel)) {
+            KeLog.warnChannel(KeRun.class, "Channel Sync {0} null", clazz.getName());
             return defaultValue;
         } else {
+            KeLog.infoChannel(KeRun.class, "Channel Sync selected {0}", channel.getClass().getName());
             return executor.apply(channel);
         }
     }
@@ -33,8 +36,10 @@ class KeRun {
                                          final Function<T, Future<O>> executor) {
         final T channel = Pocket.lookup(clazz);
         if (Objects.isNull(channel)) {
+            KeLog.warnChannel(KeRun.class, "Channel Async {0} null", clazz.getName());
             return future;
         } else {
+            KeLog.infoChannel(KeRun.class, "Channel Async selected {0}", channel.getClass().getName());
             return executor.apply(channel);
         }
     }
