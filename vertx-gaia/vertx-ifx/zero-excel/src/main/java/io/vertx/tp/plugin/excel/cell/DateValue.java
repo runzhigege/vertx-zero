43460f9e6d12a02c040f7e31a52ea2b6274630ce
Lang <silentbalanceyh@126.com>
2020-01-07 08:36:44 +0800

    Date Value adjustment

--- vertx-gaia/vertx-ifx/zero-excel/src/main/java/io/vertx/tp/plugin/excel/cell/DateValue.java
+++ vertx-gaia/vertx-ifx/zero-excel/src/main/java/io/vertx/tp/plugin/excel/cell/DateValue.java
@@ -6,21 +6,27 @@ import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DateUtil;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.TimeZone;
 
 class DateValue {
 
     static Object toNumeric(final Cell cell) {
         if (CellType.NUMERIC == cell.getCellType()) {
             if (DateUtil.isCellDateFormatted(cell)) {
-                final Date date = DateUtil.getJavaDate(cell.getNumericCellValue());
+                final Date date = DateUtil.getJavaDate(cell.getNumericCellValue(), TimeZone.getDefault());
+                /*
+                 * For 1899-12-30
+                 */
                 final LocalDateTime dateTime = Ut.toDateTime(date);
                 if (dateTime.getYear() < 1900) {
                     /*
-                     * Default time should be 1899 of year
+                     * Calculation has been put in `toTime`
                      */
-                    return dateTime.format(DateTimeFormatter.ISO_LOCAL_TIME);
+                    final LocalTime time = Ut.toTime(date);
+                    return time.format(DateTimeFormatter.ISO_LOCAL_TIME);
                 } else {
                     return date.toInstant();
                 }
