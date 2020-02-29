0bd918f90dd00d4744dc642c68c674ee8d681577
Lang <silentbalanceyh@126.com>
2020-02-29 15:33:01 +0800

    Zero upgrade

---
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/unity/jq/JooqBatch.java
@@ -0,0 +1,10 @@
+package io.vertx.up.unity.jq;
+
+/**
+ * @author lang
+ * <p>
+ * Batch operation help tools, it could help developer to process database batch operations
+ */
+class JooqBatch {
+
+}
